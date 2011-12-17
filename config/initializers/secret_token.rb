0176286fd58072e6a3bc2b86d54d97478282a190
Andrew Pasquale <andrew@elytra.net>
2011-12-16 22:07:23 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Balance::Application.config.secret_token = 'ddbcaabb8bc205bd11c89c39cdf5a7697cd01ab64a5542a80c07d4aee4dbac1cbcf59965b125f8a26ddfc7692c1a3f3016f0e84e18038ce7e2bc6428468c1cb8'
