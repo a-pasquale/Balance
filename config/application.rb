daf8f2ab0b41d8f8388fe15efe4f6de9a5460b06
Andrew Pasquale <andrew@elytra.net>
2011-12-20 21:10:23 -0500

    Use S3 storage.

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,7 @@ module Balance
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.middleware.insert 0, 'Dragonfly::Middleware', :images
   end
 end
---
+++ config/initializers/dragonfly.rb
@@ -0,0 +1,8 @@
+require 'dragonfly'
+app = Dragonfly[:images]
+
+app.configure_with(:imagemagick)
+app.configure_with(:rails)
+app.configure_with(:heroku, 'elytra') if Rails.env.production?
+
+app.define_macro(ActiveRecord::Base, :image_accessor)
