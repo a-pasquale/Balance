4c4f771034d107c6d9147907480a052b4e5c8963
Andrew Pasquale <andrew@elytra.net>
2012-01-09 22:48:57 -0500

    Lazily compile assets.

--- config/application.rb
+++ config/application.rb
@@ -4,9 +4,9 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
+  #Bundler.require(*Rails.groups(:assets => %w(development test)))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module Balance
@@ -46,6 +46,5 @@ module Balance
     config.assets.version = '1.0'
 
     config.middleware.insert 0, 'Dragonfly::Middleware', :images
-    config.assets.initialize_on_precompile = false
   end
 end
