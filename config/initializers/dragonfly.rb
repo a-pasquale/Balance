bd43971ddd43066f9775b0dfc876b3aa0da68f59
Andrew Pasquale <andrew@elytra.net>
2012-01-09 22:54:57 -0500

    Configure dragonfly to use S3 datastore.

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -4,5 +4,13 @@ app = Dragonfly[:images]
 app.configure_with(:imagemagick)
 app.configure_with(:rails)
 app.configure_with(:heroku, 'elytra') 
-
+if Rails.env.production?
+  app.configure do |c|
+    c.datastore = Dragonfly::DataStorage::S3DataStore.new(
+      :bucket_name => 'my-bucket-name',
+      :access_key_id => ENV['S3_KEY'],
+      :secret_access_key => ENV['S3_SECRET']
+    )
+  end
+end
 app.define_macro(ActiveRecord::Base, :image_accessor)
